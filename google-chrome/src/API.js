8028f63fd9a4e0c9d39a283e903df77e9c74fc59
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-24 09:29:17 +0700

    navbar google-chrome vue

---
+++ google-chrome/src/API.js
@@ -0,0 +1,4 @@
+const API_KEY =
+  "https://v16-webapp.tiktok.com/6ca69dfa620df63acfb1f297fa66ee1b/63ce7b21/video/tos/alisg/tos-alisg-pve-0037/a05206c91ba5424a9e8216e5a790add1/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=2016&bt=1008&cs=0&ds=3&ft=4b~OyMO_8Zmo0GlUb64jVWzfPpWrKsdm&mime_type=video_mp4&qs=0&rc=NDw0aDM8aGhlZjhmNTxoN0BpM3h2OzY6ZmZuPDMzODgzNEA2YF42L18yXi8xXjZjLV41YSM2Yi82cjRfa2JgLS1kLy1zcw%3D%3D&l=20230123061754BDB16534D578B077E1CF&btag=80000";
+
+export default API_KEY;
---
+++ google-chrome/src/components/Appbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="headerRight">
+    <router-link class="menu-item_menu_link" to="/"> Gmail</router-link>
+    <router-link class="menu-item_menu_link" to="/"> Images</router-link>
+    <div class="menu-item_menu">
+      <v-icon color="blue-grey darken-2"> mdi-apps</v-icon>
+    </div>
+    <div class="menu-item">
+      <v-icon color="blue-grey darken-2"> mdi-account-circle</v-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Appbar",
+  };
+</script>
+
+<style></style>
