c3d3998155ef5d8f1a0042b9b804355ff3ba3121
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-24 10:07:16 +0700

    navbar and style navbar

--- google-chrome/src/components/Appbar.vue
+++ google-chrome/src/components/Appbar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="headerRight">
-    <router-link class="menu-item_menu_link" to="/"> Gmail</router-link>
-    <router-link class="menu-item_menu_link" to="/"> Images</router-link>
-    <div class="menu-item_menu">
+    <router-link class="menu-item _menu _link" to="/"> Gmail</router-link>
+    <router-link class="menu-item _menu _link" to="/"> Images</router-link>
+    <div class="menu-item _menu">
       <v-icon color="blue-grey darken-2"> mdi-apps</v-icon>
     </div>
     <div class="menu-item">
@@ -17,4 +17,35 @@
   };
 </script>
 
-<style></style>
+<style scoped>
+  .headerRight {
+    display: flex;
+    font-size: 13px;
+    color: rgba(0, 0, 0, 0.87);
+    position: relative;
+    right: 0;
+    top: 15px;
+    z-index: 2;
+  }
+  .menu-item {
+    padding-top: 7px;
+    padding: 5px;
+  }
+  .menu-item._menu {
+    padding-top: 13px;
+  }
+  .menu-item._menu._link {
+    padding-top: 16px;
+  }
+
+  a:visited {
+    color: rgba(0, 0, 0, 0.87);
+  }
+  a {
+    color: rgba(0, 0, 0, 0.87);
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
